2ac7dcff700c92cef42f24c58cc43b266dcf84ce
janitoorion <alejandro.comas@gmail.com>
2016-08-30 18:54:59 -0300

    asdf

--- application/views/Lock/v_lock.php
+++ application/views/Lock/v_lock.php
@@ -64,14 +64,14 @@
 
 			<form class="lockscreen animated flipInY" action="index.html#ajax/dashboard.html">
 				<div class="logo">
-					<h1 class="semi-bold"><img src="assets/img/logo-o.png" alt="" /> SmartAdmin</h1>
+					<h1 class="semi-bold"><img src="assets/img/logo-o.png" alt="" /> Seguros Generales </h1>
 				</div>
 				<div>
-					<img src="assets/img/avatars/sunny-big.png" alt="" width="120" height="120" />
+					<img src="assets/img/avatars/male.png" alt="" width="120" height="120" />
 					<div>
-						<h1><i class="fa fa-user fa-3x text-muted air air-top-right hidden-mobile"></i>John Doe <small><i class="fa fa-lock text-muted"></i> &nbsp;Locked</small></h1>
+						<h1><i class="fa fa-user fa-3x text-muted air air-top-right hidden-mobile"></i> <?= $rememberData['nombre']; ?> <small><i class="fa fa-lock text-muted"></i> &nbsp;bloqueado</small></h1>
 						<p class="text-muted">
-							<a href="mailto:simmons@smartadmin">john.doe@smartadmin.com</a>
+							
 						</p>
 
 						<div class="input-group">
@@ -83,13 +83,13 @@
 							</div>
 						</div>
 						<p class="no-margin margin-top-5">
-							Logged as someone else? <a href="login.html"> Click here</a>
+							Entrar como alguien mas? <a href="login.html"> Click aca</a>
 						</p>
 					</div>
 
 				</div>
 				<p class="font-xs margin-top-5">
-					Copyright SmartAdmin 2014-2020.
+					
 
 				</p>
 			</form>
