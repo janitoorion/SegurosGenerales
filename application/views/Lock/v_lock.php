edddba18bf16efc8aa985537313f2dc9648591c7
janitoorion <alejandro.comas@gmail.com>
2016-08-31 10:34:48 -0300

    asdf

--- application/views/Lock/v_lock.php
+++ application/views/Lock/v_lock.php
@@ -62,7 +62,7 @@
 
 			<!-- MAIN CONTENT -->
 
-			<form class="lockscreen animated flipInY" action="index.html#ajax/dashboard.html">
+			<form class="lockscreen animated flipInY" action="LoginController/validar_login" method="POST" id="lock-form">
 				<div class="logo">
 					<h1 class="semi-bold"><img src="assets/img/logo-o.png" alt="" /> Seguros Generales </h1>
 				</div>
@@ -71,11 +71,14 @@
 					<div>
 						<h1><i class="fa fa-user fa-3x text-muted air air-top-right hidden-mobile"></i> <?= $rememberData['nombre']; ?> <small><i class="fa fa-lock text-muted"></i> &nbsp;bloqueado</small></h1>
 						<p class="text-muted">
-							
+							Ingrese los datos para ingresar al sistema
 						</p>
 
 						<div class="input-group">
-							<input class="form-control" type="password" placeholder="Password">
+							<input class="form-control" type="hidden" name="username" id="username" value="<?= $rememberData['usuario']; ?>">
+							<input class="form-control" type="password" name="password" id="password" placeholder="Contraseña" type="submit">
+							<br>
+							<input class="form-control" type="hidden" name="remember" id="remember" value="on">
 							<div class="input-group-btn">
 								<button class="btn btn-primary" type="submit">
 									<i class="fa fa-key"></i>
@@ -83,7 +86,7 @@
 							</div>
 						</div>
 						<p class="no-margin margin-top-5">
-							Entrar como alguien mas? <a href="login.html"> Click aca</a>
+							Entrar como alguien mas? <a href="LoginController"> Click aca</a>
 						</p>
 					</div>
 
@@ -126,8 +129,69 @@
 			
 		<![endif]-->
 
+		<!-- CUSTOM NOTIFICATION -->
+		<script src="assets/js/notification/SmartNotification.min.js"></script>
+
 		<!-- MAIN APP JS FILE -->
 		<script src="assets/js/app.min.js"></script>
-
+		<script type="text/javascript">
+			runAllForms();
+            pageSetUp();
+            
+			var pagefunction = function() {
+				// Validation
+				$("#lock-form").validate({
+					rules : {
+						password : {
+							required : true,
+							minlength : 3,
+							maxlength : 20
+						}
+					},
+					messages : {
+						password : {
+							required : 'Ingrese Contraseña'
+						}
+					},
+					errorPlacement : function(error, element) {
+						error.insertAfter(element.parent());
+					},
+                    submitHandler: function(form) {
+                        $.ajax({
+                            url: form.action,
+                            type: form.method,
+                            data: $(form).serialize(),
+                            dataType: "json",
+                            success: function (data) {
+                                if (data['STATUS'] == "TRUE") {
+                                        window.location.href = 'HomeController';
+                                    }
+                                else{
+                                    $.smallBox({
+                                        title : 'Atención',
+                                        content : "<i class='fa fa-info-circle'></i><i> " + data['STATUS'] + "</i>",
+                                        color : "#C46A69",
+                                        timeout: 2000,
+                                        icon : "fa fa-thumbs-down bounce animated",
+                                    });
+                                }
+                            },
+                            error: function (xhr, ajaxOptions, thrownError) {
+                                $.smallBox({
+                                    title : 'Atención',
+                                    content : "<i class='fa fa-info'></i><i> " + thrownError + "</i>",
+                                    color : "#C46A69",
+                                    timeout: 2000,
+                                    icon : "fa fa-thumbs-down bounce animated",
+                                });
+                            }                                                
+                        });
+                    }
+                    
+				});
+			}
+            
+            loadScript("assets/js/plugin/bootstrap-progressbar/bootstrap-progressbar.min.js", pagefunction);
+		</script>
 	</body>
 </html>
--- application/views/Login/v_login.php
+++ application/views/Login/v_login.php
@@ -1,252 +1,204 @@
 <!DOCTYPE html>
-<html lang="en-us" id="extr-page">
-	<head>
-		<meta charset="utf-8">
-		<title> SmartAdmin</title>
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-		
-		<!-- #CSS Links -->
-		<!-- Basic Styles -->
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url() ?>assets/css/font-awesome.min.css">
+<html lang="en-us" id="lock-page">
 
-		<!-- SmartAdmin Styles : Caution! DO NOT change the order -->
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url() ?>assets/css/smartadmin-production-plugins.min.css">
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url() ?>assets/css/smartadmin-production.min.css">
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url() ?>assets/css/smartadmin-skins.min.css">
+<head>
+  <meta charset="utf-8">
+  <title> Seguros Generales </title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-		<!-- SmartAdmin RTL Support -->
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url() ?>assets/css/smartadmin-rtl.min.css"> 
+  <!-- #CSS Links -->
+  <!-- Basic Styles -->
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/font-awesome.min.css">
 
-		<!-- We recommend you use "your_style.css" to override SmartAdmin
-		     specific styles this will also ensure you retrain your customization with each SmartAdmin update.
-		<link rel="stylesheet" type="text/css" media="screen" href="css/your_style.css"> -->
+  <!-- SmartAdmin Styles : Caution! DO NOT change the order -->
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/smartadmin-production-plugins.min.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/smartadmin-production.min.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/smartadmin-skins.min.css">
 
-		<!-- Demo purpose only: goes with demo.js, you can delete this css when designing your own WebApp -->
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url() ?>assets/css/demo.min.css">
+  <!-- SmartAdmin RTL Support -->
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/smartadmin-rtl.min.css">
 
-		<!-- #FAVICONS -->
-		<link rel="shortcut icon" href="<?php echo base_url() ?>assets/img/favicon/favicon.ico" type="image/x-icon">
-		<link rel="icon" href="<?php echo base_url() ?>assets/img/favicon/favicon.ico" type="image/x-icon">
+  <!-- We recommend you use "your_style.css" to override SmartAdmin
+specific styles this will also ensure you retrain your customization with each SmartAdmin update.
+<link rel="stylesheet" type="text/css" media="screen" href="css/your_style.css"> -->
 
-		<!-- #GOOGLE FONT -->
-		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
+  <!-- Demo purpose only: goes with demo.js, you can delete this css when designing your own WebApp -->
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/demo.min.css">
 
-		<!-- #APP SCREEN / ICONS -->
-		<!-- Specifying a Webpage Icon for Web Clip 
-			 Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html -->
-		<link rel="apple-touch-icon" href="<?php echo base_url() ?>assets/img/splash/sptouch-icon-iphone.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url() ?>assets/img/splash/touch-icon-ipad.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url() ?>assets/img/splash/touch-icon-iphone-retina.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url() ?>assets/img/splash/touch-icon-ipad-retina.png">
-		
-		<!-- iOS web-app metas : hides Safari UI Components and Changes Status Bar Appearance -->
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-status-bar-style" content="black">
-		
-		<!-- Startup image for web apps -->
-		<link rel="apple-touch-startup-image" href="<?php echo base_url() ?>assets/img/splash/ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
-		<link rel="apple-touch-startup-image" href="<?php echo base_url() ?>assets/img/splash/ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
-		<link rel="apple-touch-startup-image" href="<?php echo base_url() ?>assets/img/splash/iphone.png" media="screen and (max-device-width: 320px)">
+  <!-- page related CSS -->
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/lockscreen.min.css">
 
-	</head>
-	
-	<body class="animated fadeInDown">
+  <!-- #FAVICONS -->
+  <link rel="shortcut icon" href="assets/img/favicon/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="assets/img/favicon/favicon.ico" type="image/x-icon">
 
-		<header id="header">
+  <!-- #GOOGLE FONT -->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
 
-			<div id="logo-group">
-				<!--<span id="logo"> <img src="<?php echo base_url() ?>assets/img/logo.png" alt="SmartAdmin"> </span>-->
-			</div>
+  <!-- #APP SCREEN / ICONS -->
+  <!-- Specifying a Webpage Icon for Web Clip
+    Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html -->
+  <link rel="apple-touch-icon" href="assets/img/splash/sptouch-icon-iphone.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/splash/touch-icon-ipad.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/img/splash/touch-icon-iphone-retina.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/img/splash/touch-icon-ipad-retina.png">
 
-			
+  <!-- iOS web-app metas : hides Safari UI Components and Changes Status Bar Appearance -->
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-		</header>
+  <!-- Startup image for web apps -->
+  <link rel="apple-touch-startup-image" href="assets/img/splash/ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
+  <link rel="apple-touch-startup-image" href="assets/img/splash/ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
+  <link rel="apple-touch-startup-image" href="assets/img/splash/iphone.png" media="screen and (max-device-width: 320px)">
 
-		<div id="main" role="main">
+</head>
 
-			<!-- MAIN CONTENT -->
-			<div id="content" class="container">
+<body>
 
-				<div class="row">
-					<div class="col-xs-12 col-sm-12 col-md-7 col-lg-4 hidden-xs hidden-sm">
-						<!--
-						<h1 class="txt-color-red login-header-big">SmartAdmin</h1>
-						<div class="hero">
+  <div id="main" role="main">
 
-							<div class="pull-left login-desc-box-l">
-								<h4 class="paragraph-header">It's Okay to be Smart. Experience the simplicity of SmartAdmin, everywhere you go!</h4>
-								<div class="login-app-icons">
-									<a href="javascript:void(0);" class="btn btn-danger btn-sm">Frontend Template</a>
-									<a href="javascript:void(0);" class="btn btn-danger btn-sm">Find out more</a>
-								</div>
-							</div>
-							
-							<img src="<?php echo base_url() ?>assets/img/demo/iphoneview.png" class="pull-right display-image" alt="" style="width:210px">
+    <!-- MAIN CONTENT -->
 
-						</div>
+    <form class="lockscreen animated flipInY" action="LoginController/validar_login" method="POST" id="lock-form">
+      <div class="logo">
+        <h1 class="semi-bold"><img src="assets/img/logo-o.png" alt="" /> Seguros Generales </h1>
+      </div>
+      <div>
+        <img src="assets/img/avatars/male.png" alt="" width="120" height="120" />
+        <div>
+          <h1><i class="fa fa-user fa-3x text-muted air air-top-right hidden-mobile"></i> Acceso al sistema <small><i class="fa fa-lock text-muted"></i> &nbsp;bloqueado</small></h1>
+          <p class="text-muted">
+            Ingrese los datos para entrar al sistema
+          </p>
 
-						<div class="row">
-							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-								<h5 class="about-heading">About SmartAdmin - Are you up to date?</h5>
-								<p>
-									Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.
-								</p>
-							</div>
-							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-								<h5 class="about-heading">Not just your average template!</h5>
-								<p>
-									Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi voluptatem accusantium!
-								</p>
-							</div>
-						</div>
-						-->
+          <div class="input-group col-md-12">
+            <input class="form-control" type="text" name="username" id="username" value="" placeholder="Usuario" style="margin-top:5px">
+            <input class="form-control" type="password" name="password" id="password" placeholder="Contraseña" type="submit" style="margin-top:5px">
+          </div>
 
-					</div>
-					<div class="col-xs-12 col-sm-12 col-md-5 col-lg-4">
-						<div class="well no-padding">
-							<form action="LoginController/validar_login" id="login-form" class="smart-form client-form" method="POST">
-								<header>
-									Login
-								</header>
-								<fieldset>
-									<section>
-										<label class="label">Usuario</label>
-										<label class="input"> <i class="icon-append fa fa-user"></i>
-											<input type="text" name="username" id="username">
-											<b class="tooltip tooltip-top-right"><i class="fa fa-user txt-color-teal"></i> </b></label>
-									</section>
-
-									<section>
-										<label class="label">Password</label>
-										<label class="input"> <i class="icon-append fa fa-lock"></i>
-											<input type="password" name="password">
-											<b class="tooltip tooltip-top-right"><i class="fa fa-lock txt-color-teal"></i> Ingresa tu password</b> </label>
-										<div class="note">
-											<a href="forgotpassword.html">Olvidaste el password?</a>
-										</div>
-									</section>
-
-									<section>
-										<label class="checkbox">
-											<input type="checkbox" name="remember" checked="">
-											<i></i>Mantener conectado</label>
-									</section>
-								</fieldset>
-								<footer>
-									<button type="submit" class="btn btn-primary">
-										Entrar
-									</button>
-								</footer>
-							</form>
-
-						</div>
-					</div>
-				</div>
-			</div>
-
+          <fieldset>
+            <section>
+			<div class="col-md-12">
+              <label class="checkbox" >
+                <input type="checkbox" name="remember" checked="" style="margin-left:-10px">
+                <i></i> &nbsp; Seguir conectado
+              </label>
+			  </div>
+            </section>
+          </fieldset>
+		  <div class="input-group col-md-12 text-align-right">
+			<button type="submit" class="btn btn-primary">
+				Entrar
+			</button>
 		</div>
-
-		<!--================================================== -->	
-
-		<!-- PACE LOADER - turn this on if you want ajax loading to show (caution: uses lots of memory on iDevices)-->
-		<script src="<?php echo base_url() ?>assets/js/plugin/pace/pace.min.js"></script>
-
-	    <!-- Link to Google CDN's jQuery + jQueryUI; fall back to local -->
-	    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script> if (!window.jQuery) { document.write('<script src="js/libs/jquery-2.1.1.min.js"><\/script>');} </script>
-
-	    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-		<script> if (!window.jQuery.ui) { document.write('<script src="js/libs/jquery-ui-1.10.3.min.js"><\/script>');} </script>
-
-		<!-- IMPORTANT: APP CONFIG -->
-		<script src="<?php echo base_url() ?>assets/js/app.config.js"></script>
-
-		<!-- JS TOUCH : include this plugin for mobile drag / drop touch events 		
-		<script src="js/plugin/jquery-touch/jquery.ui.touch-punch.min.js"></script> -->
-
-		<!-- BOOTSTRAP JS -->		
-		<script src="<?php echo base_url() ?>assets/js/bootstrap/bootstrap.min.js"></script>
-
-		<!-- JQUERY VALIDATE -->
-		<script src="<?php echo base_url() ?>assets/js/plugin/jquery-validate/jquery.validate.min.js"></script>
+        </div>
 		
-		<!-- JQUERY MASKED INPUT -->
-		<script src="<?php echo base_url() ?>assets/js/plugin/masked-input/jquery.maskedinput.min.js"></script>
-		
-		<!--[if IE 8]>
-			
-			<h1>Your browser is out of date, please update your browser by going to www.microsoft.com/download</h1>
-			
-		<![endif]-->
+      </div>
+      <p class="font-xs margin-top-5">
 
-		<!-- MAIN APP JS FILE -->
-		<script src="<?php echo base_url() ?>assets/js/app.min.js"></script>
 
-		<script type="text/javascript">
-			runAllForms();
+      </p>
+    </form>
 
-			var pagefunction = function() {
-				$("#login-form").validate({
-					rules : {
-						username : {
-							required : true
-						},
-						password : {
-							required : true,
-							minlength : 3,
-							maxlength : 20
-						}
-					},
-					messages : {
-						username : {
-							required : '<?= $this->lang->line('porfavorIngreseRut', FALSE) ?>'
-						},
-						password : {
-							required : '<?= $this->lang->line('porfavorIngreseContraseña', FALSE) ?>'
-						}
-					},
-					errorPlacement : function(error, element) {
-						error.insertAfter(element.parent());
-					},
-                    submitHandler: function(form) {
-                    	$.ajax({
-                            url: form.action,
-                            type: form.method,
-                            data: $(form).serialize(),
-                            dataType: "json",
-                            success: function (data) {
-                                if (data['STATUS'] == "TRUE") {
-                                        window.location.href = 'HomeController';
-                                    }
-                                else{
-                                    $.smallBox({
-                                        title : '<?= $this->lang->line('atencion', FALSE) ?>',
-                                        content : data['STATUS'],
-                                        color : "#C46A69",
-                                        timeout: 2000,
-                                        icon : "fa fa-warning shake animated",
-                                    });
-                                }
-                            },
-                            error: function (xhr, ajaxOptions, thrownError) {
-                                $.smallBox({
-                                        title : '<?= $this->lang->line('atencion', FALSE) ?>',
-                                        content : thrownError,
-                                        color : "#C46A69",
-                                        timeout: 2000,
-                                        icon : "fa fa-warning shake animated",
-                                });
-                            }                                                
-                        });
-                    }
-                    
-				});
-			};
-			loadScript("assets/js/plugin/bootstrap-progressbar/bootstrap-progressbar.min.js", pagefunction);
-		</script>
+  </div>
 
-	</body>
+  <!--================================================== -->
+
+  <!-- PACE LOADER - turn this on if you want ajax loading to show (caution: uses lots of memory on iDevices)-->
+  <script src="assets/js/plugin/pace/pace.min.js"></script>
+
+  <!-- Link to Google CDN's jQuery + jQueryUI; fall back to local -->
+  <script src="assets/js/libs/jquery-2.1.1.min.js"></script>
+  <script src="assets/js/libs/jquery-ui-1.10.3.min.js"></script>
+
+  <!-- IMPORTANT: APP CONFIG -->
+  <script src="assets/js/app.config.js"></script>
+
+  <!-- JS TOUCH : include this plugin for mobile drag / drop touch events
+    <script src="js/plugin/jquery-touch/jquery.ui.touch-punch.min.js"></script> -->
+
+  <!-- BOOTSTRAP JS -->
+  <script src="assets/js/bootstrap/bootstrap.min.js"></script>
+
+  <!-- JQUERY VALIDATE -->
+  <script src="assets/js/plugin/jquery-validate/jquery.validate.min.js"></script>
+
+  <!-- JQUERY MASKED INPUT -->
+  <script src="assets/js/plugin/masked-input/jquery.maskedinput.min.js"></script>
+
+  <!--[if IE 8]>
+    
+<h1>Your browser is out of date, please update your browser by going to www.microsoft.com/download</h1>
+
+<![endif]-->
+
+  <!-- CUSTOM NOTIFICATION -->
+  <script src="assets/js/notification/SmartNotification.min.js"></script>
+
+  <!-- MAIN APP JS FILE -->
+  <script src="assets/js/app.min.js"></script>
+  <script type="text/javascript">
+    runAllForms();
+    pageSetUp();
+
+    var pagefunction = function() {
+      // Validation
+      $("#lock-form").validate({
+        rules: {
+          password: {
+            required: true,
+            minlength: 3,
+            maxlength: 20
+          }
+        },
+        messages: {
+          password: {
+            required: 'Ingrese Contraseña'
+          }
+        },
+        errorPlacement: function(error, element) {
+          error.insertAfter(element.parent());
+        },
+        submitHandler: function(form) {
+          $.ajax({
+            url: form.action,
+            type: form.method,
+            data: $(form).serialize(),
+            dataType: "json",
+            success: function(data) {
+              if (data['STATUS'] == "TRUE") {
+                window.location.href = 'HomeController';
+              } else {
+                $.smallBox({
+                  title: 'Atención',
+                  content: "<i class='fa fa-info-circle'></i><i> " + data['STATUS'] + "</i>",
+                  color: "#C46A69",
+                  timeout: 2000,
+                  icon: "fa fa-thumbs-down bounce animated",
+                });
+              }
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+              $.smallBox({
+                title: 'Atención',
+                content: "<i class='fa fa-info'></i><i> " + thrownError + "</i>",
+                color: "#C46A69",
+                timeout: 2000,
+                icon: "fa fa-thumbs-down bounce animated",
+              });
+            }
+          });
+        }
+
+      });
+    }
+
+    loadScript("assets/js/plugin/bootstrap-progressbar/bootstrap-progressbar.min.js", pagefunction);
+  </script>
+</body>
+
 </html>
